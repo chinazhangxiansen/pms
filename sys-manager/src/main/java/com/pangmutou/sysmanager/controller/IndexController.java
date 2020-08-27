78cf7c888171e64df21ec4c0a9808342e9c13db3
zhangxiansen <1248123371@qq.com>
2020-08-27 10:07:37 +0800

    对一些未使用的引用的去除

--- sys-manager/src/main/java/com/pangmutou/sysmanager/controller/IndexController.java
+++ sys-manager/src/main/java/com/pangmutou/sysmanager/controller/IndexController.java
@@ -4,13 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/index/")
 public class IndexController {
 
     @ResponseBody
     @RequestMapping("demo")
-    public String demo(){
-        return "success";
+    public String demo(HttpServletRequest request){
+        System.out.println("111");
+        return "{\"ret_code\":\"1\",\"msg\":\"success\"}";
     }
 }
--- sys-manager/src/main/java/com/pangmutou/sysmanager/controller/KnowledgeController.java
+++ sys-manager/src/main/java/com/pangmutou/sysmanager/controller/KnowledgeController.java
@@ -1,9 +1,8 @@
 package com.pangmutou.sysmanager.controller;
 
+import com.pangmutou.common.utils.GsonUtil;
 import com.pangmutou.sysmanager.bean.Knowledge;
 import com.pangmutou.sysmanager.service.impl.KnowledgeServiceImpl;
-import com.pangmutou.common.utils.GsonUtil;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
